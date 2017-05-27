4c371ca8a60cdc2438e97ee5c2bd1761b7e81b0a
wenzhenglin <wenzhenglin@haodai.net>
2017-05-27 19:02:53 +0800

    Add example of supporting debug output on the fly.

---
+++ doc/support-debug-on-the-fly.go
@@ -0,0 +1,38 @@
+// Example of turn debug on the fly
+//      $ kill -s SIGUSR1 prog
+package main
+
+import (
+        "os"
+        "os/signal"
+        "syscall"
+        "time"
+
+        "git.clwen.com/golib/log"
+)
+
+func main() {
+        for {
+                log.Println(time.Now())
+                log.Debug.Println("debug:", time.Now())
+                time.Sleep(1 * time.Second)
+        }
+}
+
+func init() {
+        c := make(chan os.Signal, 1)
+        signal.Notify(c, syscall.SIGUSR1)  //SIGUSR1 doesn't work on windows
+        go func() {
+                for _ = range c {
+                        var level string
+                        switch log.GetLevel() {
+                        case "debug":
+                                level = "info"
+                        default:
+                                level = "debug"
+                        }
+                        log.SetLevel(level)
+                        log.Println("got signal, set log level to ", level)
+                }
+        }()
+}
